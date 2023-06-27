752a511583547f9409ce264ec8c47cdc2ea3055f
Md Abbas Uddin <gmabbas44@gmail.com>
2023-06-28 02:19:47 +0600

    update: setup http server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log('Hello World, from TypeScript!')
+import express from 'express';
+import http from "http"
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import compression from 'compression';
+
+
+const app = express()
+
+app.use(cors({
+    credentials: true,
+}))
+
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(compression())
+
+const server = http.createServer(app)
+
+server.listen(3000, () => {
+    console.log('Server is running on port http://localhost:3000/')
+})
